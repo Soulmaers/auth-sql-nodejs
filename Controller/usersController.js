0315607c7cf6afddf7a6d148243ec28fb2c73c7c
Soulmaers <certiss@ya.ru>
2022-11-02 22:28:18 +0300

    регисистрация

--- Controller/usersController.js
+++ Controller/usersController.js
@@ -1,32 +1,57 @@
 const response = require('../response')
 const db = require('../settings/db')
+const bcrypt = require('bcryptjs')
 
+exports.getAllUsers = (req, res) => {
 
-exports.users = (req, res) => {
-
-    db.query('SELECT * FROM `users`', (error, rows, fields) => {
+    db.query('SELECT `id`, `name`, `second_name`, `email` FROM `users`', (error, rows, fields) => {
         if (error) {
-            console.log(error)
+            response.status(404, error, res)
         }
         else {
-            response.status(rows, res)
+            response.status(200, rows, res)
         }
     })
 
 }
 
-exports.add = (req, res) => {
+exports.signup = (req, res) => {
 
-    const sql = "INSERT INTO `users`(`name`, `second_name`,`email`) VALUES('" + req.query.name + "','" + req.query.second_name + "','" + req.query.email + "')"
-
-    db.query(sql, (error, result) => {
+    db.query("SELECT `id`, `email`, `name` FROM `users` WHERE `email`='" + req.body.email + "'", (error, rows, field) => {
         if (error) {
-            console.log(error)
+            response.status(404, res)
+        } else if (typeof rows !== 'undefined' && rows.length > 0) {
+            //сonsole.log(rows)
+            const row = JSON.parse(JSON.stringify(rows))
+            row.map(rw => {
+                response.status(404, { message: `Пользователь с таким email- ${rw.email} уже есть` }, res)
+                return true
+            })
+
+
         }
         else {
-            response.status(result, res)
+            const email = req.body.email
+            const name = req.body.name
+            const secondName = req.body.second_name !== '' ? req.body.second_name : ''
+            const salt = bcrypt.genSaltSync(15)
+            const password = bcrypt.hashSync(req.body.password, salt)
+
+            const sql = "INSERT INTO `users`(`name`, `second_name`,`email`, `password`) VALUES('" + name + "', '" + secondName + "', '" + email + "', '" + password + "')"
+            db.query(sql, (error, result) => {
+                if (error) {
+                    response.status(400, error, res)
+                }
+                else {
+                    response.status(200, { message: `Регистрация прошла успешно`, result }, res)
+                }
+            })
         }
 
     })
+
+
+
+
 
 }
--- settings/routes.js
+++ settings/routes.js
@@ -4,8 +4,16 @@ module.exports = (app) => {
     const indexController = require('../Controller/indexController')
     const usersController = require('../Controller/usersController')
 
-    app.route('/').get(indexController.index)
-    app.route('/users').get(usersController.users)
-    app.route('/users/add').post(usersController.add)
+    app
+        .route('/')
+        .get(indexController.index)
+
+    app
+        .route('/api/users')
+        .get(usersController.getAllUsers)
+
+    app
+        .route('/api/auth/signup')
+        .post(usersController.signup)
 
 }
